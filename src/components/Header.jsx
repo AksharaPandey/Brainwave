a8318e8a6e290f05dceca02001e38e58fafea906
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-20 15:16:07 +0530

    Fixed logo import and updated Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import brainwave from "../assets/brainwave.svg"; // <-- Specify the file name here
 
 const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-      Header
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4"></div>
+      <a>
+        <img src={brainwave} alt="Brainwave logo" />
+      </a>
     </div>
   );
 };
