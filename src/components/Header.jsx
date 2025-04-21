c7e7d73958b2efb00f88c845b74349f3de790430
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-21 13:32:24 +0530

    Section updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,36 @@ import React, { useState } from "react";
 import brainwave from "../assets/brainwave.svg";
 import { navigation } from "../constants";
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header"; // fixed typo
 
 const Header = () => {
   const { hash } = useLocation(); // just get hash
-  const [openNavigation, setOpenNavigation] = useState(true);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
-    setOpenNavigation(!openNavigation);
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
   };
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 backdrop-blur-sm 
-        ${openNavigation ? "bg-n-8" : "bg-n-8/90"} 
-        lg:bg-n-8/90 lg:backdrop-blur-sm`}
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
@@ -35,6 +48,7 @@ const Header = () => {
               <a
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1
                   ${item.onlyMobile ? "lg:hidden" : ""}
                   px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold
