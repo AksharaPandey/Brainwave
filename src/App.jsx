36fa9ca7f3e76097264cf8c6bd5251da3615ab00
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-21 14:02:14 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header.jsx"; // <-- add this line
 import Button from "./components/Button.jsx";
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-
+import Hero from "./components/Hero.jsx";
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
