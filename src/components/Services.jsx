225ef036deed35c9438ae7b8939ce32672379641
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-24 18:02:31 +0530

    Updated Service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Section from "./Section";
 import Heading from "./Heading";
-import { service1 } from "../assets";
-
+import { brainwave, check, service1 } from "../assets";
+import { brainwaveServices } from "../constants";
 const Services = () => {
   return (
     <Section id="how-to-use">
@@ -21,6 +21,23 @@ const Services = () => {
                 height={730}
                 src={service1}
               />
+            </div>
+            <div className="relative z-1 max-w-[17rem] ml-auto">
+              <h4 className="h4 mb-4">Smartest AI</h4>
+              <p className="body-2 mb-[3rem] text-n-3">
+                Brainwave unlocks the potential of AI-powered applications
+              </p>
+              <ul className="body-2">
+                {brainwaveServices.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start py-4 border-t border-n-6"
+                  >
+                    <img width={24} height={24} src={check} />
+                    <p className="ml-4">{item}</p>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
