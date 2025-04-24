81d97517e921a8b198c4424c58811ecbca42705a
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2025-04-24 17:30:15 +0530

    Services

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Hero from "./components/Hero.jsx";
 import Benefits from "./components/Benefits.jsx";
 import Collaboration from "./components/Collaboration.jsx";
+import Services from "./components/Services.jsx";
 const App = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
